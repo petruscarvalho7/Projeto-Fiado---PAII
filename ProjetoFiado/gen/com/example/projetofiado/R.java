d4b959d42c315c0fb46ee1627f5227a32ce5fc18
Petrus Carvalho <petrus.carvalho@dce.ufpb.br>
2013-12-06 10:31:04 -0300

    Telas projeto atualizado.

--- ProjetoFiado/gen/com/example/projetofiado/R.java
+++ ProjetoFiado/gen/com/example/projetofiado/R.java
@@ -8,7 +8,15 @@
 package com.example.projetofiado;
 
 public final class R {
+    public static final class array {
+        public static final int arrayNumeroParcelas=0x7f070001;
+        public static final int arrayPagamento=0x7f070000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int default_color=0x7f050001;
+        public static final int pressed_color=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,90 +25,104 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg_key=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int BtProximo=0x7f080011;
-        public static final int ETxtAno=0x7f080017;
-        public static final int ETxtBairro=0x7f08001f;
-        public static final int ETxtCep=0x7f080021;
-        public static final int ETxtCidade=0x7f08001e;
-        public static final int ETxtCnpj=0x7f080025;
-        public static final int ETxtCpf=0x7f080013;
-        public static final int ETxtDdd=0x7f08000d;
-        public static final int ETxtDia=0x7f080015;
-        public static final int ETxtEmail=0x7f08000f;
-        public static final int ETxtEndereco=0x7f08001c;
-        public static final int ETxtLembreteSenha=0x7f080024;
-        public static final int ETxtMes=0x7f080016;
-        public static final int ETxtNome=0x7f080012;
-        public static final int ETxtNomeUsuario=0x7f080026;
-        public static final int ETxtNumero=0x7f08001d;
-        public static final int ETxtPais=0x7f080020;
-        public static final int ETxtReferecia=0x7f080010;
-        public static final int ETxtRepetirSenha=0x7f080023;
-        public static final int ETxtRg=0x7f080014;
-        public static final int ETxtSenha=0x7f080022;
-        public static final int ETxtTelefone=0x7f08000e;
-        public static final int RBFeminino=0x7f08001a;
-        public static final int RBMasculino=0x7f080019;
-        public static final int RGSexo=0x7f080018;
-        public static final int action_settings=0x7f080029;
-        public static final int botaoMaior=0x7f080004;
-        public static final int botaoTeste=0x7f080000;
-        public static final int btEsqueceuSenha=0x7f080027;
-        public static final int btLogin=0x7f080028;
-        public static final int btProximo=0x7f08001b;
-        public static final int campoCodigo=0x7f080008;
-        public static final int campoNome=0x7f080006;
-        public static final int campoPreco=0x7f08000c;
-        public static final int campoTipo=0x7f08000a;
-        public static final int fotoContatoTelaCliente=0x7f080001;
-        public static final int fotoContatoTelaPrincipal=0x7f080003;
-        public static final int lbCodigo=0x7f080007;
-        public static final int lbNome=0x7f080005;
-        public static final int lbPreco=0x7f08000b;
-        public static final int lbTipo=0x7f080009;
-        public static final int nomeUsuario=0x7f080002;
+        public static final int BtProximo=0x7f0a0011;
+        public static final int ETxtAno=0x7f0a0017;
+        public static final int ETxtBairro=0x7f0a001f;
+        public static final int ETxtCep=0x7f0a0021;
+        public static final int ETxtCidade=0x7f0a001e;
+        public static final int ETxtCnpj=0x7f0a0025;
+        public static final int ETxtCpf=0x7f0a0013;
+        public static final int ETxtDdd=0x7f0a000d;
+        public static final int ETxtDia=0x7f0a0015;
+        public static final int ETxtEmail=0x7f0a000f;
+        public static final int ETxtEndereco=0x7f0a001c;
+        public static final int ETxtLembreteSenha=0x7f0a0024;
+        public static final int ETxtMes=0x7f0a0016;
+        public static final int ETxtNome=0x7f0a0012;
+        public static final int ETxtNomeUsuario=0x7f0a0028;
+        public static final int ETxtNumero=0x7f0a001d;
+        public static final int ETxtPais=0x7f0a0020;
+        public static final int ETxtReferecia=0x7f0a0010;
+        public static final int ETxtRepetirSenha=0x7f0a0023;
+        public static final int ETxtRg=0x7f0a0014;
+        public static final int ETxtSenha=0x7f0a0022;
+        public static final int ETxtTelefone=0x7f0a000e;
+        public static final int RBFeminino=0x7f0a001a;
+        public static final int RBMasculino=0x7f0a0019;
+        public static final int RGSexo=0x7f0a0018;
+        public static final int action_settings=0x7f0a002e;
+        public static final int botaoMaior=0x7f0a0004;
+        public static final int botaoTeste=0x7f0a0000;
+        public static final int btAdd=0x7f0a002b;
+        public static final int btDelete=0x7f0a002c;
+        public static final int btEsqueceuSenha=0x7f0a0029;
+        public static final int btLogin=0x7f0a002a;
+        public static final int btProximo=0x7f0a001b;
+        public static final int campoCodigo=0x7f0a0008;
+        public static final int campoNome=0x7f0a0006;
+        public static final int campoPreco=0x7f0a000c;
+        public static final int campoTipo=0x7f0a000a;
+        public static final int fotoContatoTelaCliente=0x7f0a0001;
+        public static final int fotoContatoTelaPrincipal=0x7f0a0003;
+        public static final int lbCodigo=0x7f0a0007;
+        public static final int lbNome=0x7f0a0005;
+        public static final int lbPreco=0x7f0a000b;
+        public static final int lbTipo=0x7f0a0009;
+        public static final int lvProdutos=0x7f0a002d;
+        public static final int nomeUsuario=0x7f0a0002;
+        public static final int spPagamento=0x7f0a0026;
+        public static final int spParcelas=0x7f0a0027;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int menu_cliente=0x7f030001;
         public static final int menu_principal=0x7f030002;
         public static final int menu_produto=0x7f030003;
-        public static final int tela_cadastro_produto=0x7f030004;
-        public static final int tela_cadastrocliente_final=0x7f030005;
-        public static final int tela_cadastrocliente_primeira=0x7f030006;
-        public static final int tela_cadastrocliente_segunda=0x7f030007;
-        public static final int tela_cadastrovendedor_final=0x7f030008;
-        public static final int tela_cadastrovendedor_primeira=0x7f030009;
-        public static final int tela_cadastrovendedor_segunda=0x7f03000a;
-        public static final int tela_login=0x7f03000b;
+        public static final int menu_venda=0x7f030004;
+        public static final int tela_cadastro_produto=0x7f030005;
+        public static final int tela_cadastrocliente_final=0x7f030006;
+        public static final int tela_cadastrocliente_primeira=0x7f030007;
+        public static final int tela_cadastrocliente_segunda=0x7f030008;
+        public static final int tela_cadastrovendedor_final=0x7f030009;
+        public static final int tela_cadastrovendedor_primeira=0x7f03000a;
+        public static final int tela_cadastrovendedor_segunda=0x7f03000b;
+        public static final int tela_formapagamento=0x7f03000c;
+        public static final int tela_login=0x7f03000d;
+        public static final int tela_novavenda=0x7f03000e;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040026;
-        public static final int app_name=0x7f040025;
+        public static final int action_settings=0x7f04002a;
+        public static final int app_name=0x7f040029;
+        public static final int btAdd=0x7f040026;
         public static final int btBuscar=0x7f040007;
         public static final int btCadastrarNovoUser=0x7f040024;
+        public static final int btDelete=0x7f040027;
         public static final int btEsqueceuSenha=0x7f040022;
         public static final int btExcluir=0x7f040008;
+        public static final int btFinalizar=0x7f040028;
         public static final int btGerenciarClientes=0x7f040001;
         public static final int btGerenciarProdutos=0x7f040002;
         public static final int btGerenciarVendas=0x7f040000;
         public static final int btLogin=0x7f040023;
+        public static final int btNovaVenda=0x7f040025;
         public static final int btNovoCadastroCliente=0x7f040006;
         public static final int btProximo=0x7f04000e;
         public static final int btSair=0x7f040003;
         public static final int btSalvar=0x7f04000d;
-        public static final int cliente=0x7f04002d;
-        public static final int infoCadastral=0x7f040029;
+        public static final int cliente=0x7f040032;
+        public static final int formaDePagamento=0x7f040036;
+        public static final int infoCadastral=0x7f04002d;
         public static final int lbBairro=0x7f040017;
         public static final int lbCPF=0x7f04000f;
         public static final int lbCadastroCliente=0x7f040005;
@@ -124,15 +146,18 @@ public final class R {
         public static final int lbSenha=0x7f04001f;
         public static final int lbTelefone=0x7f04001b;
         public static final int lbTipo=0x7f04000b;
-        public static final int logo=0x7f04002f;
-        public static final int menuCliente=0x7f04002a;
-        public static final int menuPrincipal=0x7f040028;
-        public static final int menuProduto=0x7f04002b;
-        public static final int nomeUsuario=0x7f040027;
-        public static final int novoCadastro=0x7f04002c;
+        public static final int logo=0x7f040034;
+        public static final int menuCliente=0x7f04002e;
+        public static final int menuPrincipal=0x7f04002c;
+        public static final int menuProduto=0x7f04002f;
+        public static final int menuVenda=0x7f040030;
+        public static final int nomeEntradaPagamento=0x7f040035;
+        public static final int nomeUsuario=0x7f04002b;
+        public static final int novoCadastro=0x7f040031;
+        public static final int numeroParcelas=0x7f040037;
         public static final int rbFeminino=0x7f040012;
         public static final int rbMasculino=0x7f040011;
-        public static final int vendedor=0x7f04002e;
+        public static final int vendedor=0x7f040033;
     }
     public static final class style {
         /** 
@@ -156,10 +181,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
