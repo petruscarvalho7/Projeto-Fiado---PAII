78f4ed5942a2b10c9e1df9388323417e38ee6b42
robson100 <robson.soares@dce.ufpb.br>
2013-12-06 16:19:04 -0300

    Adicão de background

--- src/com/pa2/projeto/MainActivity.java
+++ src/com/pa2/projeto/MainActivity.java
@@ -9,9 +9,9 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		//setContentView(R.layout.activity_login);
 		//setContentView(R.layout.activity_cadastrocliente1);
-		//setContentView(R.layout.activity_cadastrocliente2);
+		setContentView(R.layout.activity_cadastrocliente2);
 		//setContentView(R.layout.activity_cadastrocliente3);
 		//setContentView(R.layout.activity_cadastrovendedor1);
 		//setContentView(R.layout.activity_cadastrovendedor2);
