05863708559062f7c81426cdd6ee33e7f32f7a1c
Petrus Carvalho <petrus.carvalho@dce.ufpb.br>
2014-03-03 13:22:38 -0300

    Atualizacao ProjetoFiado: Adicionado Repositorio Clientes utilizando Content Provider

--- ProjetoFiado/src/com/example/projetofiado/negocio/Cliente.java
+++ ProjetoFiado/src/com/example/projetofiado/negocio/Cliente.java
@@ -2,8 +2,17 @@ package com.example.projetofiado.negocio;
 
 import java.util.Date;
 
-public class Cliente {
+import android.net.Uri;
+import android.provider.BaseColumns;
 
+public class Cliente {
+	
+	public static String[] colunas = new String[]{ Clientes._ID , Clientes.NOME, Clientes.CPF, Clientes.SEXO, Clientes.TELEFONE, Clientes.EMAIL, Clientes.REFERENCIA, 
+		Clientes.NASCIMENTO, Clientes.ENDERECO};
+	
+ 
+	public static final String AUTHORITY ="com.example.projetofiado.negocio.cliente";
+	long id;	
 	private String nome, cpf, rg, sexo, telefone, email, referencia;
 	private Date nascimento;
 	private Endereco endereco;
@@ -76,6 +85,42 @@ public class Cliente {
 		this.referencia = referencia;
 	}
 	
+	public static final class Clientes implements BaseColumns{
 	
+		
+		//N‹o instanciar!!!
+		private Clientes(){
+			
+		}
+		
+		//content://com.example.projetofiado.negocio.cliente/clientes
+		public static final Uri CONTENT_URI = Uri.parse("content://"+ AUTHORITY +"/clientes");
+		
+		//Mime Type para todos os clientes
+		public static final String CONTENT_TYPE ="vnd.android.cursor.dir/vnd.google.clientes";
+		
+		//Mime Type para unico cliente
+		public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.google.clientes";
+		
+		//Ordenacao default para inserir no order by
+		public static final String DEFAULT_SORT_ORDER = "_id ASC";
+		
+		//Campos
+		public static final String NOME = "nome"; 
+		public static final String CPF = "cpf";
+		public static final String ENDERECO = "endereco";
+
+		public static final String NASCIMENTO = "nascimento";
+
+		public static final String REFERENCIA = "referencia";
+
+		public static final String EMAIL = "email";
+
+		public static final String TELEFONE = "telefone";
+
+		public static final String SEXO = "sexo";
+		
+		
+	}
 	
 }
